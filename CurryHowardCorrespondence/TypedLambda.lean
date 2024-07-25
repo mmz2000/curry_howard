038f81ef2c88481717b63df5f907f8d82dcc2a42
mammadz <mohammadmahdizareyi@gmail.com>
2024-07-25 17:17:44 +0330

    fix: refactor whole thing and omit judgement

--- CurryHowardCorrespondence/TypedLambda.lean
+++ CurryHowardCorrespondence/TypedLambda.lean
@@ -35,9 +35,8 @@ inductive Terms : Type
 | Inl : Terms -> Types -> Types -> Terms
 | Inr : Terms -> Types -> Types -> Terms
 | Case : Terms -> Terms -> Terms -> Terms
-| Subs : Terms -> Terms -> Terms -> Terms
 | Unit : Terms
-| Empty : Terms
+| EmptyElim : Terms ->  Terms -> Terms
 
 
 def Terms.toString : Terms → String
@@ -50,26 +49,12 @@ def Terms.toString : Terms → String
 | Terms.Inl e t1 t2 => "inl " ++ e.toString ++ " as " ++ t1.toString ++ " + " ++ t2.toString
 | Terms.Inr e t1 t2 => "inr " ++ e.toString ++ " as " ++ t1.toString ++ " + " ++ t2.toString
 | Terms.Case e1 e2 e3 => "case " ++ e1.toString ++ " of " ++ e2.toString ++ " | " ++ e3.toString
-| Terms.Subs e1 e2 e3 => e1.toString ++ "[" ++ e2.toString ++ " := " ++ e3.toString ++ "]"
 | Terms.Unit => "()"
-| Terms.Empty => "⊥"
+| Terms.EmptyElim a b => "EmptyElim " ++ a.toString ++ " as " ++ b.toString
 
 inductive Context : Type
 | Empty : Context
 | Cons : String -> Types -> Context -> Context
-
-inductive Judgement : Context → Terms → Types → Type
-| Var : ∀ {Γ : Context} {x : String} {A : Types}, Context → Terms → Types →  Judgement Γ (Terms.Var x A) A
-| Abs : ∀ {Γ : Context} {x : String} {A : Types} {e : Terms} {B : Types}, Judgement (Context.Cons x A Γ) e B → Judgement Γ (Terms.Abs x A e) (Types.Arrow A B)
-| App : ∀ {Γ : Context} {e1 e2 : Terms} {A B : Types}, Judgement Γ e1 (Types.Arrow A B) → Judgement Γ e2 A → Judgement Γ (Terms.App e1 e2) B
-| Pair : ∀ {Γ : Context} {e1 e2 : Terms} {A B : Types}, Judgement Γ e1 A → Judgement Γ e2 B → Judgement Γ (Terms.Pair e1 e2) (Types.Touples A B)
-| Fst : ∀ {Γ : Context} {e : Terms} {A B : Types}, Judgement Γ e (Types.Touples A B) → Judgement Γ (Terms.Fst e) A
-| Snd : ∀ {Γ : Context} {e : Terms} {A B : Types}, Judgement Γ e (Types.Touples A B) → Judgement Γ (Terms.Snd e) B
-| Inl : ∀ {Γ : Context} {e : Terms} {A B : Types}, Judgement Γ e A → Judgement Γ (Terms.Inl e A B) (Types.Either A B)
-| Inr : ∀ {Γ : Context} {e : Terms} {A B : Types}, Judgement Γ e B → Judgement Γ (Terms.Inr e A B) (Types.Either A B)
-| Case : ∀ {Γ : Context} {e e1 e2 : Terms} {A B C : Types}, Judgement Γ e (Types.Either A B) → Judgement Γ e1 (Types.Arrow A C) → Judgement Γ e2 (Types.Arrow B C) → Judgement Γ (Terms.Case e e1 e2) C
-| Unit : ∀ {Γ : Context}, Judgement Γ Terms.Unit Types.Unit
-| EmptyElim : ∀ {Γ : Context} {e : Terms}, Judgement Γ e Types.Empty → Judgement Γ (Terms.Empty) A
 
 
 def Context.toString : Context → String
@@ -80,65 +65,109 @@ def Context.getTypeOf : Context →  String → Option Types
 | Context.Empty ,_ => none
 | Context.Cons x A Γ ,y => if x = y then some A else Context.getTypeOf Γ y
 
-def Judgement.toString : ∀ {Γ : Context} {e : Terms} {A : Types}, Judgement Γ e A → String
-| Γ ,Terms.Var x A ,_ ,Judgement.Var _ _ _=>Γ.toString ++ Γ.toString ++ x ++ " : " ++ A.toString
-| Γ ,Terms.Abs x A _ ,_ ,Judgement.Abs proof =>Γ.toString ++ "λ" ++ x ++ " : " ++ A.toString ++ " . " ++ proof.toString
-| Γ ,Terms.App _ _ ,_ ,Judgement.App proof1 proof2 =>Γ.toString ++ "(" ++ proof1.toString ++ " " ++ proof2.toString ++ ")"
-| Γ ,Terms.Pair _ _ ,_ ,Judgement.Pair proof1 proof2 =>Γ.toString ++ "(" ++ proof1.toString ++ ", " ++ proof2.toString ++ ")"
-| Γ ,Terms.Fst _ ,_ ,Judgement.Fst proof =>Γ.toString ++ "fst " ++ proof.toString
-| Γ ,Terms.Snd _ ,_ ,Judgement.Snd proof =>Γ.toString ++ "snd " ++ proof.toString
-| Γ ,Terms.Inl _ A B ,_ ,Judgement.Inl proof =>Γ.toString ++ "inl " ++ proof.toString ++ " as " ++ A.toString ++ " + " ++ B.toString
-| Γ ,Terms.Inr _ A B ,_ ,Judgement.Inr proof =>Γ.toString ++ "inr " ++ proof.toString ++ " as " ++ A.toString ++ " + " ++ B.toString
-| Γ ,Terms.Case _ _ _ ,_ ,Judgement.Case proof1 proof2 proof3 =>Γ.toString ++ "case " ++ proof1.toString ++ " of " ++ proof2.toString ++ " | " ++ proof3.toString
-| Γ ,Terms.Unit ,_ ,Judgement.Unit =>Γ.toString ++ "()"
-| Γ ,Terms.Empty ,_ ,Judgement.EmptyElim proof =>Γ.toString ++ proof.toString
+def Terms.GetIsInhabitant : Context → Terms → Types → Bool
+| Γ, Terms.Var x A, B => match Context.getTypeOf Γ x with
+                        | some C => EqualTypes A C && EqualTypes A B
+                        | none => false
+| Γ, Terms.Abs x A e, Types.Arrow B C => EqualTypes A B && (Terms.GetIsInhabitant (Context.Cons x A Γ) e C || A == Types.Empty)
+| Γ, Terms.App e1 e2, C => match e1 with
+                          | Terms.Abs x A _ => Terms.GetIsInhabitant Γ e2 A && Terms.GetIsInhabitant (Context.Cons x A Γ) e1 (Types.Arrow A C)
+                          | _ => True
+| Γ, Terms.Pair e1 e2, Types.Touples A B => Terms.GetIsInhabitant Γ e1 A && Terms.GetIsInhabitant Γ e2 B
+| Γ, Terms.Fst e, A => match e with
+                      | Terms.Pair e1 _ => Terms.GetIsInhabitant Γ e1 A
+                      | _ => false
+| Γ, Terms.Snd e, B => match e with
+                      | Terms.Pair _ e2 => Terms.GetIsInhabitant Γ e2 B
+                      | _ => false
+| Γ, Terms.Inl e A B, Types.Either C D => Terms.GetIsInhabitant Γ e A && EqualTypes A C && EqualTypes B D
+| Γ, Terms.Inr e A B, Types.Either C D => Terms.GetIsInhabitant Γ e B && EqualTypes A C && EqualTypes B D
+| Γ, Terms.Case e e1 e2, C => match e with
+                            | Terms.Inl e' A B => Terms.GetIsInhabitant Γ e1 (Types.Arrow A C) && Terms.GetIsInhabitant Γ e2 (Types.Arrow B C) && Terms.GetIsInhabitant Γ e' A
+                            | Terms.Inr e' A B => Terms.GetIsInhabitant Γ e1 (Types.Arrow A C) && Terms.GetIsInhabitant Γ e2 (Types.Arrow B C) && Terms.GetIsInhabitant Γ e' B
+                            | Terms.Var x L => match Context.getTypeOf Γ x with
+                                  | some (Types.Either A B) => Terms.GetIsInhabitant Γ e1 (Types.Arrow A C) && Terms.GetIsInhabitant Γ e2 (Types.Arrow B C) && L == (Types.Either A B)
+                                  | _ => false
+                            | _ => false
+| _, Terms.Unit, Types.Unit => true
+| Γ, Terms.EmptyElim e1 _, _ => Terms.GetIsInhabitant Γ e1 Types.Empty
+| _, _, _ => false
 
-def getTypeOfTerm : Terms →  Option Types
-| Terms.Var _ A => A
-| Terms.Abs _ A e => match getTypeOfTerm e with
-                      | some B => some (Types.Arrow A B)
-                      | none => none
-| Terms.App e1 e2 => match getTypeOfTerm e1, getTypeOfTerm e2 with
-                      | some (Types.Arrow A B), some C => if A == C then some B else none
-                      | _,_ => none
-| Terms.Pair e1 e2 => match getTypeOfTerm e1, getTypeOfTerm e2 with
-                      | some A, some B => some (Types.Touples A B)
-                      | _,_ => none
-| Terms.Fst e => match getTypeOfTerm e with
-                  | some (Types.Touples A _) => some A
-                  | _ => none
-| Terms.Snd e => match getTypeOfTerm e with
-                  | some (Types.Touples _ B) => some B
-                  | _ => none
-| Terms.Inl e _ B => match getTypeOfTerm e with
-                      | some A => some (Types.Either A B)
-                      | _ => none
-| Terms.Inr e A _ => match getTypeOfTerm e with
-                      | some B => some (Types.Either A B)
-                      | _ => none
-| Terms.Case e e1 e2 => match getTypeOfTerm e with
-                        | some (Types.Either A B) => match getTypeOfTerm e1, getTypeOfTerm e2 with
-                                                      | some (Types.Arrow K C), some (Types.Arrow J D) => if C == D && (
-                                                        (A==K && B==J )|| (A==J && B==K)
-                                                      ) then some C else none
-                                                      | _,_ => none
-                        | _ => none
-| Terms.Unit => some Types.Unit
-| Terms.Empty => none
-| Terms.Subs e1 e2 e3 => if getTypeOfTerm e2 == getTypeOfTerm e3 then getTypeOfTerm e1 else none
+-- simple example of x: A ⊢ x : A
+
+def example1 : Context := Context.Cons "x" (Types.TypeVar "A") Context.Empty
+def example1Term : Terms := Terms.Var "x" (Types.TypeVar "A")
+
+#eval example1Term.toString
+#eval Terms.GetIsInhabitant example1 example1Term (Types.TypeVar "A")
+
+-- simple example of ⊢ λx:A.x : A → A
+
+def example2 : Context := Context.Empty
+def example2Term : Terms := Terms.Abs "x" (Types.TypeVar "A") (Terms.Var "x" (Types.TypeVar "A"))
+
+#eval example2Term.toString
+#eval Terms.GetIsInhabitant example2 example2Term (Types.Arrow (Types.TypeVar "A") (Types.TypeVar "A"))
+
+-- simple example of "l : C, k : C, a : (A + B),  ⊢ (λx : (A + B) . case x : (A + B) of λy : A . k : C | λz : B . l : C a : (A + B)) : C"
 
 
-def CheckJudgement : Judgement Γ T t →  Bool
-| Judgement.Var Γ T t => match Γ.getTypeOf T.toString with
-                      | some j =>  j == t
-                      | none => False
-| Judgement.Abs T => CheckJudgement T
-| Judgement.App T1 T2 => CheckJudgement T1 && CheckJudgement T2
-| Judgement.Pair T1 T2 => CheckJudgement T1 && CheckJudgement T2
-| Judgement.Fst T => CheckJudgement T
-| Judgement.Snd T => CheckJudgement T
-| Judgement.Inl T => CheckJudgement T
-| Judgement.Inr T => CheckJudgement T
-| Judgement.Case T1 T2 T3 => CheckJudgement T1 && CheckJudgement T2 && CheckJudgement T3
-| Judgement.Unit => true
-| Judgement.EmptyElim T => !CheckJudgement T
+def example3 : Context := Context.Empty
+def example3Term : Terms := Terms.Abs "x"
+                                      (
+                                        Types.Either (Types.TypeVar "A") (Types.TypeVar "B")
+                                      )
+                                      (
+                                        Terms.Case (
+                                                    Terms.Var "x" (
+                                                      Types.Either (Types.TypeVar "A") (Types.TypeVar "B")
+                                                      )
+                                                    )
+                                                    (
+                                                      Terms.Abs "y"
+                                                                (Types.TypeVar "A")
+                                                                (Terms.Var "k" (Types.TypeVar "C"))
+                                                    )
+                                                    (
+                                                      Terms.Abs "z"
+                                                                (Types.TypeVar "B")
+                                                                (Terms.Var "l" (Types.TypeVar "C"))
+                                                    )
+                                      )
+def example4 : Context := Context.Cons "l" (Types.TypeVar "C") (Context.Cons "k" (Types.TypeVar "C") (Context.Cons "a" (Types.Either (Types.TypeVar "A") (Types.TypeVar "B")) Context.Empty))
+def example4Term : Terms := Terms.App example3Term (Terms.Var "a" (Types.Either (Types.TypeVar "A") (Types.TypeVar "B")))
+
+#eval example3.toString ++ " ⊢ " ++example3Term.toString
+#eval Terms.GetIsInhabitant example3 example3Term (Types.Arrow (Types.Either (Types.TypeVar "A") (Types.TypeVar "B")) (Types.TypeVar "C"))
+#eval example4.toString ++ " ⊢ " ++ example4Term.toString
+#eval Terms.GetIsInhabitant example4 example4Term (Types.TypeVar "C")
+
+-- simple example of exfalso quodlibet
+def example5 : Context := Context.Cons "a" Types.Empty Context.Empty
+def example5Term : Terms := Terms.EmptyElim (Terms.Var "a" Types.Empty) (Terms.Var "b" (Types.TypeVar "A"))
+
+#eval example5.toString ++ " ⊢ " ++ example5Term.toString
+#eval Terms.GetIsInhabitant example5 example5Term (Types.TypeVar "A")
+
+def example6 : Context := Context.Empty
+def example6Term : Terms := Terms.Abs "x" (Types.Empty) (Terms.Var "b" (Types.TypeVar "A"))
+#eval example6.toString ++ " ⊢ " ++ example6Term.toString
+#eval Terms.GetIsInhabitant example6 example6Term (Types.Arrow Types.Empty (Types.TypeVar "A"))
+
+-- simple example of a->b->a
+def example7 : Context := Context.Empty
+def example7Term : Terms := Terms.Abs "x" (Types.TypeVar "A") (Terms.Abs "y" (Types.TypeVar "B") (Terms.Var "x" (Types.TypeVar "A")))
+#eval example7.toString ++ " ⊢ " ++ example7Term.toString
+#eval Terms.GetIsInhabitant example7 example7Term (Types.Arrow (Types.TypeVar "A") (Types.Arrow (Types.TypeVar "B") (Types.TypeVar "A")))
+
+-- simple example of a->a
+def example8 : Context := Context.Empty
+def example8Term : Terms := Terms.Abs "x" (Types.TypeVar "A") (Terms.Var "x" (Types.TypeVar "A"))
+#eval example8.toString ++ " ⊢ " ++ example8Term.toString
+#eval Terms.GetIsInhabitant example8 example8Term (Types.Arrow (Types.TypeVar "A") (Types.TypeVar "A"))
+
+-- simple example of a->b fail cause b is not habitet
+def example9 : Context := Context.Empty
+def example9Term : Terms := Terms.Abs "x" (Types.TypeVar "A") (Terms.Var "x" (Types.TypeVar "B"))
+#eval example9.toString ++ " ⊢ " ++ example9Term.toString
+#eval Terms.GetIsInhabitant example9 example9Term (Types.Arrow (Types.TypeVar "A") (Types.TypeVar "B"))
