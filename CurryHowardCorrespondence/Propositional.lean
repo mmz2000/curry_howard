96200aecf8c6ea5fe9a6cd251c862d50e7db5a8e
mammadz <mohammadmahdizareyi@gmail.com>
2024-07-31 16:06:05 +0330

    fix: refactor typed lambda and propositional logic to correct it

--- CurryHowardCorrespondence/Propositional.lean
+++ CurryHowardCorrespondence/Propositional.lean
@@ -1,19 +1,5 @@
-inductive PropVar : Type
-| fromString : String → PropVar
-
-def PropVar.eq : PropVar → PropVar → Bool
-| PropVar.fromString s1, PropVar.fromString s2 => s1 == s2
-
--- BEq instance for PropVar
-instance : BEq PropVar := ⟨PropVar.eq⟩
-
-def PropVar.toString : PropVar → String
-| PropVar.fromString s => s
-
-instance : ToString PropVar := ⟨PropVar.toString⟩
-
 inductive Formula : Type
-| Var : PropVar → Formula
+| Var : String → Formula
 | And : Formula → Formula → Formula
 | Or : Formula → Formula → Formula
 | Impl : Formula → Formula → Formula
@@ -30,82 +16,22 @@ def Formula.eq : Formula → Formula → Bool
 -- BEq instance for Formula
 instance : BEq Formula := ⟨Formula.eq⟩
 
-
-inductive Proof : Type
-| Axiom : Formula → Proof
-| AndIntro : Formula → Proof → Proof → Proof
-| AndElim1 : Formula → Proof → Proof
-| AndElim2 : Formula → Proof → Proof
-| OrIntro1 : Formula → Proof → Proof
-| OrIntro2 : Formula → Proof → Proof
-| OrElim : Formula → Proof → Proof → Proof → Proof
-| ImplIntro : Formula → Proof → Proof
-| ImplElim : Formula → Proof → Proof → Proof
-| FalsumElim : Formula → Proof → Proof
-
-
 def Formula.toString : Formula → String
-| Formula.Var v => v.toString
+| Formula.Var v => v
 | Formula.And p q => "(" ++ toString p ++ " ∧ " ++ toString q ++ ")"
 | Formula.Or p q => "(" ++ toString p ++ " ∨ " ++ toString q ++ ")"
 | Formula.Impl p q => "(" ++ toString p ++ " → " ++ toString q ++ ")"
 | Formula.Falsum => "⊥"
 
 
-def listFormulaToString : List Formula → String
-| [] => ""
-| [f] => f.toString
-| f :: fs => f.toString ++ ", " ++ listFormulaToString fs
-
-def Proof.toString : Proof → String
-| Proof.Axiom f => "Axiom " ++ f.toString
-| Proof.AndIntro f p q => "AndIntro " ++ f.toString ++ "by " ++ p.toString ++ " and " ++ q.toString
-| Proof.AndElim1 f p => "AndElim1 " ++ f.toString ++ " by " ++ p.toString
-| Proof.AndElim2 f p => "AndElim2 " ++ f.toString ++ " by " ++ p.toString
-| Proof.OrIntro1 f p => "OrIntro1 " ++ f.toString ++ " by " ++ p.toString
-| Proof.OrIntro2 f p => "OrIntro2 " ++ f.toString ++ " by " ++ p.toString
-| Proof.OrElim f p q r => "OrElim " ++ f.toString ++ " by " ++ p.toString ++ " and " ++ q.toString ++ " and " ++ r.toString
-| Proof.ImplIntro f p => "ImplIntro " ++ f.toString ++ " by " ++ p.toString
-| Proof.ImplElim f p q => "ImplElim " ++ f.toString ++ " by " ++ p.toString ++ " and " ++ q.toString
-| Proof.FalsumElim f p => "FalsumElim " ++ f.toString ++ " by " ++ p.toString
-
-def Proof.getFormula : Proof → Formula
-| Proof.Axiom f => f
-| Proof.AndIntro f _ _ => f
-| Proof.AndElim1 f _ => f
-| Proof.AndElim2 f _ => f
-| Proof.OrIntro1 f _ => f
-| Proof.OrIntro2 f _ => f
-| Proof.OrElim f _ _ _ => f
-| Proof.ImplIntro f _ => f
-| Proof.ImplElim f _ _ => f
-| Proof.FalsumElim f _ => f
-
-
-def Proof.Check : Proof → List Formula → Bool
-| Proof.Axiom f, fs => fs.contains f
-| Proof.AndIntro f p q, fs => Proof.Check p fs && Proof.Check q fs && match f with
-  | Formula.And f1 f2 => f1 == p.getFormula && f2 == q.getFormula
-  | _ => false
-| Proof.AndElim1 f p, fs => Proof.Check p fs && match getFormula p with
-  | Formula.And f1 _ => f1 == f
-  | _ => false
-| Proof.AndElim2 f p, fs => Proof.Check p fs && match getFormula p with
-  | Formula.And _ f2 => f2 == f
-  | _ => false
-| Proof.OrIntro1 f p, fs => Proof.Check p fs && match f with
-  | Formula.Or f1 _ => f1 == p.getFormula
-  | _ => false
-| Proof.OrIntro2 f p, fs => Proof.Check p fs && match f with
-  | Formula.Or _ f2 => f2 == p.getFormula
-  | _ => false
-| Proof.OrElim f p q r, fs =>Proof.Check r fs && match getFormula r with
-  | Formula.Or f1 f2 => Proof.Check p (fs.append ([f1])) && Proof.Check q (fs.append ([f2])) && (f == getFormula p || Formula.Impl f1 f == getFormula p) && (f == getFormula q || Formula.Impl f2 f == getFormula q)
-  | _ => false
-| Proof.ImplIntro f p, fs => match f with
-  | Formula.Impl f1 f2 => Proof.Check p (fs.append ([f1])) && f2 == getFormula p
-  | _ => false
-| Proof.ImplElim f p q, fs => match getFormula p with
-  | Formula.Impl f1 f2 => Proof.Check p fs && Proof.Check q fs && f1 == getFormula q && f2 == f
-  | _ => false
-| Proof.FalsumElim f p, fs => Proof.Check p fs && Formula.Falsum == getFormula p
+inductive Provable : List Formula → Formula → Prop
+| Axiom {Γ f} (h : Γ.contains f) : Provable Γ f
+| AndIntor {Γ p q}  (h1 : Provable Γ p) (h2: Provable Γ q) : Provable Γ (Formula.And p q)
+| AndElim1 {Γ p q} (h : Provable Γ (Formula.And p q)) : Provable Γ p
+| AndElim2 {Γ p q} (h : Provable Γ (Formula.And p q)) : Provable Γ q
+| OrIntro1 {Γ p q} (h : Provable Γ p) : Provable Γ (Formula.Or p q)
+| OrIntro2 {Γ p q} (h : Provable Γ q) : Provable Γ (Formula.Or p q)
+| OrElim {Γ p q r} (h1 : Provable Γ (Formula.Or p q)) (h2 : Provable (Γ.concat p) r) (h3 : Provable (Γ.concat q) r) : Provable Γ r
+| ImpElim {Γ p q} (h1 : Provable Γ (Formula.Impl p q)) (h2 : Provable Γ q) : Provable Γ q
+| ImpIntro {Γ p q} (h : Provable (Γ.concat p) q) : Provable Γ (Formula.Impl p q)
+| false_elim {Γ p} (h : Provable Γ Formula.Falsum) : Provable Γ p
--- CurryHowardCorrespondence/TypedLambda.lean
+++ CurryHowardCorrespondence/TypedLambda.lean
@@ -3,7 +3,6 @@ inductive Types : Type
 | Arrow : Types → Types → Types
 | Touples : Types → Types → Types
 | Either : Types → Types → Types
-| Unit : Types
 | Empty : Types
 
 def EqualTypes : Types → Types → Bool
@@ -11,7 +10,6 @@ def EqualTypes : Types → Types → Bool
 | Types.Arrow t1 t2, Types.Arrow t3 t4 => EqualTypes t1 t3 && EqualTypes t2 t4
 | Types.Touples t1 t2, Types.Touples t3 t4 => EqualTypes t1 t3 && EqualTypes t2 t4
 | Types.Either t1 t2, Types.Either t3 t4 => EqualTypes t1 t3 && EqualTypes t2 t4
-| Types.Unit, Types.Unit => true
 | Types.Empty, Types.Empty => true
 | _, _ => false
 
@@ -22,35 +20,7 @@ def Types.toString : Types → String
 | Types.Arrow t1 t2 => "(" ++ t1.toString ++ " → " ++ t2.toString ++ ")"
 | Types.Touples t1 t2 => "(" ++ t1.toString ++ " × " ++ t2.toString ++ ")"
 | Types.Either t1 t2 => "(" ++ t1.toString ++ " + " ++ t2.toString ++ ")"
-| Types.Unit => "()"
 | Types.Empty => "⊥"
-
-inductive Terms : Type
-| Var : String -> Types -> Terms
-| Abs : String -> Types -> Terms -> Terms
-| App : Types -> Terms -> Terms -> Terms
-| Pair : Types -> Terms -> Terms -> Terms
-| Fst : Types -> Terms -> Terms
-| Snd : Types -> Terms -> Terms
-| Inl : Terms -> Types -> Types -> Terms
-| Inr : Terms -> Types -> Types -> Terms
-| Case : Types-> Terms -> Terms -> Terms -> Terms
-| Unit : Terms
-| EmptyElim : Types -> Terms ->  Terms -> Terms
-
-
-def Terms.toString : Terms → String
-| Terms.Var v t => v ++ " : " ++ t.toString
-| Terms.Abs v t e => "λ" ++ v ++ " : " ++ t.toString ++ " . " ++ e.toString
-| Terms.App t e1 e2 => "(" ++ e1.toString ++ ")" ++ e2.toString ++ " : " ++ t.toString
-| Terms.Pair t e1 e2 => "(" ++ e1.toString ++ ", " ++ e2.toString ++ ")" ++ " : " ++ t.toString
-| Terms.Fst t e => "fst " ++ e.toString  ++ " : " ++ t.toString
-| Terms.Snd t e => "snd " ++ e.toString ++ " : " ++ t.toString
-| Terms.Inl e t1 t2 => "inl " ++ e.toString ++ " as " ++ t1.toString ++ " + " ++ t2.toString
-| Terms.Inr e t1 t2 => "inr " ++ e.toString ++ " as " ++ t1.toString ++ " + " ++ t2.toString
-| Terms.Case t e1 e2 e3 => "case " ++ e1.toString ++ " of " ++ e2.toString ++ " | " ++ e3.toString ++ " : " ++ t.toString
-| Terms.Unit => "()"
-| Terms.EmptyElim t a b => "EmptyElim " ++ a.toString ++ " as " ++ b.toString ++ " : " ++ t.toString
 
 inductive Context : Type
 | Empty : Context
@@ -61,56 +31,31 @@ def Context.toString : Context → String
 | Context.Empty => ""
 | Context.Cons x A Γ => x ++ " : " ++ A.toString ++ ", " ++ Context.toString Γ
 
-def Context.getTypeOf : Context →  String → Option Types
-| Context.Empty ,_ => none
-| Context.Cons x A Γ ,y => if x = y then some A else Context.getTypeOf Γ y
+def Context.getType : Context → String → Option Types
+| Empty ,_ => none
+| Cons x t Γ , y => if x == y then some t else getType Γ y
 
-def Terms.getTypeOf : Context → Terms → Types
-| Γ, Terms.Var x A => match Context.getTypeOf Γ x with
-                        | some B => if B == A then A else Types.Empty
-                        | none => Types.Empty
-| Γ, Terms.Abs x A e => match Terms.getTypeOf (Context.Cons x A Γ) e with
-                        | B =>  (Types.Arrow A B)
-| Γ, Terms.App _ e1 e2 => match Terms.getTypeOf Γ e1 with
-                          | Types.Arrow A B => if Terms.getTypeOf Γ e2 == some A then B else Types.Empty
-                          | _ => Types.Empty
-| Γ, Terms.Pair t e1 e2 => match t with
-                          | Types.Touples A B => if Terms.getTypeOf Γ e1 == some A && Terms.getTypeOf Γ e2 == some B then t else Types.Empty
-                          | _ => Types.Empty
-| Γ, Terms.Fst _ e => match Terms.getTypeOf Γ e with
-                          | (Types.Touples A _) => A
-                          | _ => Types.Empty
-| Γ, Terms.Snd _ e => match Terms.getTypeOf Γ e with
-                          | (Types.Touples _ B) => B
-                          | _ => Types.Empty
-| Γ, Terms.Inl e A B => if Terms.getTypeOf Γ e == A then (Types.Either A B) else Types.Empty
-| Γ, Terms.Inr e A B => if Terms.getTypeOf Γ e == B then  (Types.Either A B) else Types.Empty
-| Γ, Terms.Case t e e1 e2 => match Terms.getTypeOf Γ e with
-                            |  (Types.Either A B) => if Terms.getTypeOf Γ e1 == some (Types.Arrow A t) && Terms.getTypeOf Γ e2 == some (Types.Arrow B t) then  t else Types.Empty
-                            | _ => Types.Empty
-| _, Terms.Unit =>  Types.Unit
-| Γ, Terms.EmptyElim t e1 e2 => if Terms.getTypeOf Γ e1 == Types.Empty && Terms.getTypeOf Γ e2 == some t then t else Types.Empty
+inductive Term : Type
+| Var : String → Types → Term
+| Abs : String → Types → Term → Term
+| App : Term → Term → Term
+| Pair : Term → Term → Term
+| Proj1 : Term → Term
+| Proj2 : Term → Term
+| Inl : Term → Types → Types → Term
+| Inr : Term → Types → Types → Term
+| Case : Term → String → Term → String → Term → Term
+| Absurd : Types → Term → Term -- absurdity elimination
 
-def Terms.GetIsInhabitant : Context → Terms → Types → Bool
-| Γ, Terms.Var x A, B => match Context.getTypeOf Γ x with
-                        | some C => EqualTypes A C && EqualTypes A B
-                        | none => B == Types.Empty
-| Γ, Terms.Abs x A e, Types.Arrow B C => EqualTypes A B && (Terms.GetIsInhabitant (Context.Cons x A Γ) e C || A == Types.Empty)
-| Γ, Terms.App t e1 e2, C => match Terms.getTypeOf Γ e1 with
-                          | Types.Arrow A B => Terms.GetIsInhabitant Γ e1 (Types.Arrow A B) && Terms.GetIsInhabitant Γ e2 A && EqualTypes B C && EqualTypes t C
-                          | _ => C == Types.Empty
-| Γ, Terms.Pair t e1 e2, Types.Touples A B => Terms.GetIsInhabitant Γ e1 A && Terms.GetIsInhabitant Γ e2 B && EqualTypes t (Types.Touples A B)
-| Γ, Terms.Fst t e, A => match Terms.getTypeOf Γ e with
-                          | Types.Touples B C => Terms.GetIsInhabitant Γ e (Types.Touples B C) && EqualTypes A B && EqualTypes t A
-                          | _ => A == Types.Empty
-| Γ, Terms.Snd t e, B => match Terms.getTypeOf Γ e with
-                          | Types.Touples A C => Terms.GetIsInhabitant Γ e (Types.Touples A C) && EqualTypes B C && EqualTypes t B
-                          | _ => B == Types.Empty
-| Γ, Terms.Inl e A B, Types.Either C D => Terms.GetIsInhabitant Γ e A && EqualTypes A C && EqualTypes B D
-| Γ, Terms.Inr e A B, Types.Either C D => Terms.GetIsInhabitant Γ e B && EqualTypes A C && EqualTypes B D
-| Γ, Terms.Case t e e1 e2, C => match Terms.getTypeOf Γ e with
-                            |  (Types.Either A B) => Terms.GetIsInhabitant Γ e (Types.Either A B) && Terms.GetIsInhabitant Γ e1 (Types.Arrow A t) && Terms.GetIsInhabitant Γ e2 (Types.Arrow B t) && EqualTypes t C
-                            | _ => C == Types.Empty
-| _, Terms.Unit, Types.Unit => true
-| Γ, Terms.EmptyElim t e1 b, c => Terms.GetIsInhabitant Γ e1 Types.Empty && Terms.GetIsInhabitant Γ b c && EqualTypes t c
-| _, _, t => t == Types.Empty
+
+inductive Inhabitable : Context → Types → Term → Prop
+| Var {Γ x A} (h : Γ.getType x = some A) : Inhabitable Γ A (Term.Var x A)
+| Abs {Γ x A B t} (h : Inhabitable (Context.Cons x A Γ) B t) : Inhabitable Γ (Types.Arrow A B) (Term.Abs x A t)
+| App {Γ A B t1 t2} (h1 : Inhabitable Γ (Types.Arrow A B) t1) (h2 : Inhabitable Γ A t2) : Inhabitable Γ B (Term.App t1 t2)
+| Pair {Γ A B t1 t2} (h1 : Inhabitable Γ A t1) (h2 : Inhabitable Γ B t2) : Inhabitable Γ (Types.Touples A B) (Term.Pair t1 t2)
+| Proj1 {Γ A B t} (h : Inhabitable Γ (Types.Touples A B) t) : Inhabitable Γ A (Term.Proj1 t)
+| Proj2 {Γ A B t} (h : Inhabitable Γ (Types.Touples A B) t) : Inhabitable Γ B (Term.Proj2 t)
+| Inl {Γ A B t} (h : Inhabitable Γ A t) : Inhabitable Γ (Types.Either A B) (Term.Inl t A B)
+| Inr {Γ A B t} (h : Inhabitable Γ B t) : Inhabitable Γ (Types.Either A B) (Term.Inr t A B)
+| Case {Γ A B C t t1 t2 x y} (h1 : Inhabitable Γ (Types.Either A B) t) (h2 : Inhabitable (Context.Cons x A Γ) C t1) (h3 : Inhabitable (Context.Cons y B Γ) C t2) : Inhabitable Γ C (Term.Case t x t1 y t2)
+| Absurd {Γ A t} (h : Inhabitable Γ Types.Empty t) : Inhabitable Γ A (Term.Absurd A t)
