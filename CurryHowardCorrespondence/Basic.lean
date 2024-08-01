eac00f1529528369ce2c47f2eb666054ccee4243
mammadz <mohammadmahdizareyi@gmail.com>
2024-08-01 19:45:47 +0330

    feat: proov p == q → (translateFormulaToType p) == (translateFormulaToType q)

--- CurryHowardCorrespondence/Basic.lean
+++ CurryHowardCorrespondence/Basic.lean
@@ -14,3 +14,153 @@ def translateTypeToFormula : Types → Formula
 | Types.Either p q => Formula.Or (translateTypeToFormula p) (translateTypeToFormula q)
 | Types.Arrow p q => Formula.Impl (translateTypeToFormula p) (translateTypeToFormula q)
 | Types.Empty => Formula.Falsum
+
+theorem dsimpfbeq: Formula.eq p q → p == q := by
+  simp [fbeq]
+  intro h
+  rw [h]
+
+theorem f2t_eq: ∀ {p q: Formula}, p == q → (translateFormulaToType p) == (translateFormulaToType q)
+| Formula.Var p, Formula.Var q => by
+  intro h
+  simp [fbeq] at h
+  simp [tbeq]
+  simp [EqualTypes]
+  simp [Formula.eq] at h
+  rw [eq_pvname_eq_tvname]
+  rw [h]
+| Formula.Var p, Formula.And _ _ => by
+  intro h
+  simp [fbeq] at h
+  simp [Formula.eq] at h
+| Formula.Var p, Formula.Impl _ _ => by
+  intro h
+  simp [fbeq] at h
+  simp [Formula.eq] at h
+| Formula.Var p, Formula.Or _ _ => by
+  intro h
+  simp [fbeq] at h
+  simp [Formula.eq] at h
+| Formula.Var p, Formula.Falsum => by
+  intro h
+  simp [fbeq] at h
+  simp [Formula.eq] at h
+| Formula.Falsum, Formula.Falsum  => by
+  simp [translateFormulaToType]
+  simp [tbeq]
+  simp [EqualTypes]
+| Formula.Falsum, Formula.Var _  => by
+  intro h
+  simp [fbeq] at h
+  simp [Formula.eq] at h
+| Formula.Falsum, Formula.And _ _  => by
+  intro h
+  simp [fbeq] at h
+  simp [Formula.eq] at h
+| Formula.Falsum, Formula.Or _ _  => by
+  intro h
+  simp [fbeq] at h
+  simp [Formula.eq] at h
+| Formula.Falsum, Formula.Impl _ _  => by
+  intro h
+  simp [fbeq] at h
+  simp [Formula.eq] at h
+| Formula.And p q, Formula.And p' q' => by
+  intro h
+  simp [translateFormulaToType]
+  simp [fbeq] at h
+  simp [Formula.eq] at h
+  let hr := h.right
+  let hl := h.left
+  simp [tbeq]
+  simp [EqualTypes]
+  let hr' := dsimpfbeq hr
+  let hl' := dsimpfbeq hl
+  let l := f2t_eq hl'
+  let r := f2t_eq hr'
+  simp [tbeq] at l
+  simp [tbeq] at r
+  simp [r]
+  simp [l]
+| Formula.And _ _, Formula.Var _ => by
+  intro h
+  simp [fbeq] at h
+  simp [Formula.eq] at h
+| Formula.And _ _, Formula.Or _ _ => by
+  intro h
+  simp [fbeq] at h
+  simp [Formula.eq] at h
+| Formula.And _ _, Formula.Impl _ _ => by
+  intro h
+  simp [fbeq] at h
+  simp [Formula.eq] at h
+| Formula.And _ _, Formula.Falsum => by
+  intro h
+  simp [fbeq] at h
+  simp [Formula.eq] at h
+| Formula.Impl p q, Formula.Impl p' q' => by
+  intro h
+  simp [translateFormulaToType]
+  simp [fbeq] at h
+  simp [Formula.eq] at h
+  let hr := h.right
+  let hl := h.left
+  simp [tbeq]
+  simp [EqualTypes]
+  let hr' := dsimpfbeq hr
+  let hl' := dsimpfbeq hl
+  let l := f2t_eq hl'
+  let r := f2t_eq hr'
+  simp [tbeq] at l
+  simp [tbeq] at r
+  simp [r]
+  simp [l]
+| Formula.Impl _ _, Formula.Var _ => by
+  intro h
+  simp [fbeq] at h
+  simp [Formula.eq] at h
+| Formula.Impl _ _, Formula.Or _ _ => by
+  intro h
+  simp [fbeq] at h
+  simp [Formula.eq] at h
+| Formula.Impl _ _, Formula.And _ _ => by
+  intro h
+  simp [fbeq] at h
+  simp [Formula.eq] at h
+| Formula.Impl _ _, Formula.Falsum => by
+  intro h
+  simp [fbeq] at h
+  simp [Formula.eq] at h
+| Formula.Or p q, Formula.Or p' q' => by
+  intro h
+  simp [translateFormulaToType]
+  simp [fbeq] at h
+  simp [Formula.eq] at h
+  let hr := h.right
+  let hl := h.left
+  simp [tbeq]
+  simp [EqualTypes]
+  let hr' := dsimpfbeq hr
+  let hl' := dsimpfbeq hl
+  let l := f2t_eq hl'
+  let r := f2t_eq hr'
+  simp [tbeq] at l
+  simp [tbeq] at r
+  simp [r]
+  simp [l]
+| Formula.Or _ _, Formula.Var _ => by
+  intro h
+  simp [fbeq] at h
+  simp [Formula.eq] at h
+| Formula.Or _ _, Formula.Impl _ _ => by
+  intro h
+  simp [fbeq] at h
+  simp [Formula.eq] at h
+| Formula.Or _ _, Formula.And _ _ => by
+  intro h
+  simp [fbeq] at h
+  simp [Formula.eq] at h
+| Formula.Or _ _, Formula.Falsum => by
+  intro h
+  simp [fbeq] at h
+  simp [Formula.eq] at h
